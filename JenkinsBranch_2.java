b728d74828266ef5410cb12a4fadb57da3512360
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-14 12:47:01 +0530

    JenkinsBranch_2.java_(v1.00)
    JenkinsBranch_2.java_(v1.00)

---
+++ JenkinsBranch_2.java
@@ -0,0 +1,9 @@
+//This is Second File
+public class JenkinsBranch_2.java {
+	
+	public static void main(String [] args)
+	{
+		System.out.println("This is from JenkinsBranch_2 Second Program - This is From Other Branch");
+		System.out.println("This is FISRT and Initial COMMIT in GitHub - From JenkinsBranch_2");
+	}
+  }
