c0d5906ebd467bfa7f6ed7842498592df8b2a652
manoranjan <manoranjan.dubey@indisoft.us>
2020-01-27 15:43:28 +0530

    k

--- src/jenkinsConcept_3/jenkinsConcept3_File_1.java
+++ src/jenkinsConcept_3/jenkinsConcept3_File_1.java
@@ -4,7 +4,7 @@ public class jenkinsConcept3_File_1 {
 	public static void main(String [] args)
 	{
 		System.out.println("jenkinsConcept3_File_1");
-		System.out.println("jenkinsConcept3_File_1: 27-Jan-2020: 3:28PM IST");
+		System.out.println("jenkinsConcept3_File_1: 27-Jan-2020: 3:28PM  IST");
 	}
 
 }
